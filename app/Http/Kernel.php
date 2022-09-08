d560d22ff6b311a1ec952eadf8e8a6a8f099725b
Sahadat Hossen <sahadatjh@gmail.com>
2022-09-08 12:40:54 +0600

    admin client freelancer middleware added

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,5 +64,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'is_admin' => \App\Http\Middleware\AdminMiddleware::class,
+        'is_client' => \App\Http\Middleware\ClientMiddleware::class,
+        'is_freelancer' => \App\Http\Middleware\FrellencerMiddleware::class,
     ];
 }
